bf271d14932e273737dcfdb8a1ad8033ea59df76
Annavaram Vinitha <annavaramvinitha@gmail.com>
2025-08-23 22:27:30 +0530

    Add files via upload

---
+++ SpringHibernateOneToManyMapping/src/main/java/com/example/service/StudentService.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.dao.IStudentDao;
+import com.example.entity.Student;
+
+@Service
+public class StudentService{
+	private final IStudentDao dao;
+
+	public StudentService(IStudentDao dao) {
+		super();
+		this.dao = dao;
+	}
+	
+	
+	public void saveData(Student student)
+	{
+		dao.saveStudent(student);
+	}
+	
+	public Student getStudent(Long id)
+	{
+		
+		return dao.getStudent(id);
+	}
+	
+}
